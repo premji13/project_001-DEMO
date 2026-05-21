2b19d5b15c42a279c5a6701e017595b1afdd5c66
premjianand <preamanand2021@gmail.com>
2026-05-21 17:44:06 +0530

    adding code to make sure the connection is alive and for recreating connections every 5 mins.

--- app/database.py
+++ app/database.py
@@ -3,7 +3,11 @@ from sqlalchemy.orm import sessionmaker
 from app.config import DATABASE_URL
 from app.models import Base
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300
+                       )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
